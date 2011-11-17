52d147657f76ef977b24c58701813bc5fbcfe45b
Christian Grlica <christian@heavenseven.net>
2011-11-17 22:07:10 +0100

    config.ru changed

--- config.ru
+++ config.ru
@@ -2,7 +2,7 @@
 require ::File.expand_path('../config/environment',  __FILE__)
 run Iwyg::Application
 
-if ENV["RAILS_ENV"] == "stage"
-  RackBaseURI /home/heaven7/rails_apps/iwyg_stage  
-  RackEnv production
-end
+#if ENV["RAILS_ENV"] == "stage"
+#  RackBaseURI /home/heaven7/rails_apps/iwyg_stage  
+#  RackEnv production
+#end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -18,10 +19,6 @@ ActiveRecord::Schema.define(:version => 20111020202307) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "item_id"
-  end
-
-  create_table "assets", :id => false, :force => true do |t|
-    t.integer "id", :null => false
   end
 
   create_table "comments", :force => true do |t|
@@ -88,13 +85,6 @@ ActiveRecord::Schema.define(:version => 20111020202307) do
 
   create_table "item_attachments", :force => true do |t|
     t.integer  "attachment_id"
-    t.integer  "item_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "item_attatchments", :force => true do |t|
-    t.integer  "parent_id"
     t.integer  "item_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -216,6 +206,17 @@ ActiveRecord::Schema.define(:version => 20111020202307) do
   add_index "rates", ["rateable_id"], :name => "index_rates_on_rateable_id"
   add_index "rates", ["user_id"], :name => "index_rates_on_user_id"
 
+  create_table "resources", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "status"
+    t.integer  "category"
+    t.integer  "user_id"
+    t.boolean  "need",        :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "searches", :force => true do |t|
     t.string   "keyword"
     t.integer  "user_id"
@@ -308,32 +309,5 @@ ActiveRecord::Schema.define(:version => 20111020202307) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
-
-  create_table "users_bak", :force => true do |t|
-    t.string   "login"
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "reset_password_token"
-    t.string   "remember_token"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",                     :default => 0
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users_bak", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users_bak", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users_bak", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users_bak", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
 
 end
