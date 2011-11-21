fe2d391ef45561e6d40d2fd96a0fb84c3c6e055d
Christian Grlica <christian@heavenseven.net>
2011-11-21 02:03:35 +0100

    Changed sign_in layout, added localized welcome email on register for user - not styled!

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,28 +1,13 @@
-class ActionMailer::Localized < ActionMailer::Base
-    private
-    # we override the template_path to render localized templates (since rails does not support that :-( )
-    # This thing is not testable since you cannot access the instance of a mailer...
-    def initialize_defaults(method_name)
-      super
-      @template = "#{I18n.locale}_#{method_name}"
-   end
-end
+class UserMailer < ActionMailer::Base
+  default :from => REPLY_EMAIL
 
-class UserMailer < ActionMailer::Localized
-  def signup_notification(user)
-    setup_email(user)
-    @subject    += I18n.translate("user.account.pleaseactivate")
-  
-    @body[:url]  = "#{HOST}/activate/#{user.activation_code}"
-  
+  def registration_notification(user)
+    @user = user
+    @url = "#{HOST}/users/sign_in"
+    email_with_name = "#{user.login} <#{user.email}>"
+    mail( :to => email_with_name, :subject => "IWYG Registration")
   end
-  
-  def activation(user)
-    setup_email(user)
-    @subject    += I18n.translate("user.account.activated")
-    @body[:url]  = "#{HOST}/"
-  end
-  
+
   def comment(user) 
     @recipients  = user[:owner].email
     @subject     = user[:subject]
--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,6 +1,6 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
-#    UserMailer.deliver_signup_notification(user)
+    UserMailer.registration_notification(user).deliver
   end
 
   def after_save(user)  
