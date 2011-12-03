fbb81af5aed5278f40cbda2ac90c1a81e9429bbb
Christian Grlica <christian@heavenseven.net>
2011-12-03 22:27:38 +0100

    registration and error explanation in user form revised

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,15 +64,15 @@ class User < ActiveRecord::Base
   
 
 
-  #validates_presence_of     :login, :email
-  #validates_presence_of     :password                   
-  #validates_presence_of     :password_confirmation      
-  #validates_length_of       :password, :within => 4..40
-  #validates_confirmation_of :password                 
-  #validates_length_of       :login,    :within => 3..40
-  #validates_length_of       :email,    :within => 3..100
-  #validates_uniqueness_of   :login, :email, :case_sensitive => false
-  #validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => :invalid
+  validates_presence_of     :login, :email
+  validates_presence_of     :password                   
+  validates_presence_of     :password_confirmation      
+  validates_length_of       :password, :within => 4..40
+  validates_confirmation_of :password                 
+  validates_length_of       :login,    :within => 3..40
+  validates_length_of       :email,    :within => 3..100
+  validates_uniqueness_of   :login, :email, :case_sensitive => false
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => :invalid
   
   # Virtual attribute for the unencrypted password
   # attr_accessor :password
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'active_record/fixtures'                                       
 
-# Fixtures.create_fixtures("#{Rails.root}/db/migrate/data", "item_types")  
-# Fixtures.create_fixtures("#{Rails.root}/db/migrate/data", "item_statuses")
+Fixtures.create_fixtures("#{Rails.root}/db/migrate/data", "item_types")  
+Fixtures.create_fixtures("#{Rails.root}/db/migrate/data", "item_statuses")
 Fixtures.create_fixtures("#{Rails.root}/db/migrate/data", "measures")   
