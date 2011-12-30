855f2acd26146a512581846829315470722ef1d9
Christian Grlica <christian@heavenseeven.net>
2011-12-30 16:55:52 +0100

    Statuses of pings seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,4 @@ require 'active_record/fixtures'
 Fixtures.create_fixtures("#{Rails.root}/db/migrate/data", "item_types")  
 Fixtures.create_fixtures("#{Rails.root}/db/migrate/data", "item_statuses")
 Fixtures.create_fixtures("#{Rails.root}/db/migrate/data", "measures")   
+Fixtures.create_fixtures("#{Rails.root}/db/migrate/data", "statuses")   
