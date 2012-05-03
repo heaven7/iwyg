58df09c2d2fc69e7104dd854ad426f53e9ce24c2
Christian Grlica <christian@heavenseven.net>
2012-05-03 21:47:55 +0200

    small fixes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -23,9 +23,9 @@ $(document).ready(function (){
       headerWidth = $('#header').css("height");
       if (headerWidth == "110px") {
         $('#header').animate({height: '310px'}, 400);
-        $('#submit-search').css("top", "143px");
+        $('#submit-search').css("top", "140px");
       } else {
-        $('#header').animate({height: '110px'}, 400);
+        $('#header').animate({height: '106px'}, 400);
         $('#submit-search').animate({opacity: '0'}, 0);
         $('#submit-search').css("top", "10px");
         $('#submit-search').animate({opacity: '1'}, {duration: 600, easing: 'linear'} );
