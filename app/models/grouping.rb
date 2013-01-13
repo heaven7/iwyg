88efefec6c213866f95a249e132aade32d493057
Christian Grlica <christian@heavenseven.net>
2013-01-13 01:23:41 +0100

    grouping observer specs

--- app/models/grouping.rb
+++ app/models/grouping.rb
@@ -1,6 +1,7 @@
 class Grouping < ActiveRecord::Base
   attr_accessible :user_id, :group_id, :accepted_at, :accepted, :owner_id
   belongs_to :user
+  belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
   belongs_to :group
 
   scope :pending, :conditions => "accepted_at is NULL"
@@ -10,9 +11,9 @@ class Grouping < ActiveRecord::Base
 		not Grouping.find_by_user_id_and_group_id(self.user_id, self.group_id).nil?
 	end
 
-  def owner
-    User.find(self.owner_id) if self.owner_id
-  end
+#  def owner
+#    User.find(self.owner_id) if self.owner_id
+#  end
 
 	def pending?
 		self.accepted_at == nil
