0389b9cb0dcd96642bfab13860c51a255c6c8ea9
Christian Grlica <christian@heavenseven.net>
2013-03-06 05:15:17 +0100

    revising mailbox_spec

--- spec/requests/mailbox_spec.rb
+++ spec/requests/mailbox_spec.rb
@@ -6,24 +6,30 @@ describe User do
 	before :each do
 		@user = create(:user, :id => Random.rand(1000))
 		login_as(@user, :scope => :user)
-		@mailbox = @user.inbox
-		visit mailbox_path(@mailbox)
-	end 
-
-	it "GET mailbox content" do
-		page.should have_content("Mailbox") 
 	end
 
-	it "GET inbox" do
-		click_link "Inbox"
-#		save_and_open_page
-		page.should have_content("Messages (0)") 
-	end
+	describe "mailbox views" do 
+		
+		before :each do
+			@mailbox = @user.inbox
+			visit mailbox_path(@mailbox)
+		end
 
-	it "GET sent messages" do
-		click_link "Sent"
-		page.should have_content("Mailbox") 
-		page.should have_content("Messages (0)") 
+		it "GET mailbox content" do
+			page.should have_content("Mailbox") 
+		end
+
+		it "GET inbox" do
+			click_link "Inbox"
+	#		save_and_open_page
+			page.should have_content("Messages (0)") 
+		end
+
+		it "GET sent messages" do
+			click_link "Sent"
+			page.should have_content("Mailbox") 
+			page.should have_content("Messages (0)") 
+		end
 	end
 
 end
