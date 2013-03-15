9ac6c455a0eefffb0ea8731829460a8b59a60f2b
Christian Grlica <christian@heavenseven.net>
2013-03-15 22:20:43 +0100

    sidekiq initializer added

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    Sidekiq.configure_client do |config|
+      config.redis = { :size => 1 }
+    end if forked
+  end
+end
