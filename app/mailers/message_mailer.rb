8d353fe197dcdad43546be5d8fcf42a57e946528
Christian Grlica <christian@heavenseven.net>
2013-03-16 00:21:35 +0100

    grrr sidekiq works on ruby 1.9.3 only

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,10 +3,12 @@ class MessageMailer < ActionMailer::Base
   default :css => MAILER_CSS
   default :charset => MAILER_CHARSET
 
-	def hasSendMessage(message, receiver, sender)
-		@receiver = receiver
-		@sender = sender
-		email_with_name = "#{@receiver.login} <#{@receiver.email}>"
-    mail( :to => email_with_name, :subject => I18n.t("mailer.message.userHasSendMessage", :user => @sender.login) )
+	def hasSendMessage(message)
+		message.recipients.each do |receiver|
+			@receiver = receiver
+			@sender = message.author
+			email_with_name = "#{@receiver.login} <#{@receiver.email}>"
+		  mail( :to => email_with_name, :subject => I18n.t("mailer.message.userHasSendMessage", :user => @sender.login) )
+		end
 	end
 end
