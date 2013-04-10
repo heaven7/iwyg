faf7bdc69c4e37bc5f59b3fa35f17a69f053e4fe
Christian Grlica <christian@heavenseven.net>
2013-04-11 00:01:56 +0200

    right dateformat for datetimepicker for events

--- config/application.rb
+++ config/application.rb
@@ -86,6 +86,7 @@ MAILER_CHARSET = "UTF-8"
 # default date/time format
 Time::DATE_FORMATS[:mailbox] = "%A, %B %d, %Y"
 Time::DATE_FORMATS[:default] = "%B %d, %Y"
+Time::DATE_FORMATS[:forms] = "%d.%m.%Y %H:%M"
 
 # hirb
 if $0 == 'irb'
