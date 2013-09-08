9e4c06414c536461d054136d0a7ab8567ad2c3b4
Christian Grlica <christian@heavenseven.net>
2013-09-08 19:42:40 +0200

    autit fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -83,10 +83,17 @@ module UsersHelper
         end
       when "Comment"
         comment = Comment.with_deleted.where(:id => audit.auditable_id).first
-        resource = comment.commentable_type.constantize.find(comment.commentable_id)
+        resource = comment.commentable
         case audit.action
         when "create"
-          link_to(t("hasCommentedOn", :title => resource.title), resource)
+					case resource.class
+					when "Item"
+						link_to(t("hasCommentedOn", :title => resource.title), resource) + resource.class
+					when "Ping"
+						link_to(t("hasCommentedOn", :title => resource.body), resource) + resource.class
+					when "Group"
+           #link_to(t("hasCommentedOn", :title => resource.title), resource)
+					end
         end
       else
 				nil
