80584c7c7cc44d0cf02425a4606175773283929f
Christian Grlica <christian@heavenseven.net>
2013-10-22 16:09:55 +0200

    email_steps for cucumber added

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,10 @@
 
 require 'cucumber/rails'
 
+# Make sure this require is after you require cucumber/rails/world.
+require 'email_spec' # add this line if you use spork
+require 'email_spec/cucumber'
+
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
