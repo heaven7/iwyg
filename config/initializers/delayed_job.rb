f7e4ec2bcded51432d699dde87a545608cf03853
Christian Grlica <christian@heavenseven.net>
2013-11-14 20:32:23 +0100

    make sure delayed jobs are running in prodcution

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -4,7 +4,7 @@ DELAYED_JOB_PID_PATH = "#{Rails.root}/tmp/pids/delayed_job.pid"
 
 def start_delayed_job
   Thread.new do 
-    `ruby script/delayed_job start`
+    'RAILS_ENV=production ruby script/delayed_job start'
   end
 end
 
